c91d4c171405d4956fdfb2fe1965ed4da4fe0434
qkr2435 <qkr2435@gmail.com>
2023-08-27 23:21:02 +0900

    add : websokethandler/loggingInterceptor

---
+++ src/main/config/RequestLoggingInterceptor.java
@@ -0,0 +1,50 @@
+package com.config;
+
+
+import com.annotation.TimeCheck;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.netty.handler.codec.http.HttpMethod;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class RequestLoggingInterceptor implements HandlerInterceptor {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        log.info("Request URL: {}", request.getRequestURL());
+        log.info("Request Method: {}", request.getMethod());
+
+        return true;
+    }
+
+    /*서블렛 로깅 찍기*/
+    @Override
+    @TimeCheck
+    public void afterCompletion(
+            HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+            throws Exception {
+
+        final ContentCachingRequestWrapper cachingRequest = (ContentCachingRequestWrapper) request;
+        final ContentCachingResponseWrapper cachingResponse = (ContentCachingResponseWrapper) response;
+
+        if (request.getMethod().equals(HttpMethod.POST)) {
+
+            log.info(
+                    "RequestBody : {} / ResponseBody : {}",
+                    objectMapper.readTree(cachingRequest.getContentAsByteArray()),
+                    objectMapper.readTree(cachingResponse.getContentAsByteArray())
+            );
+        }
+    }
+}
