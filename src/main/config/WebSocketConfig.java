f12e68298ed4a23a2f37641a72a3da632a265ea5
qkr2435 <qkr2435@gmail.com>
2023-08-27 23:20:03 +0900

    WebSocketConfig추가

---
+++ src/main/config/WebSocketConfig.java
@@ -0,0 +1,26 @@
+package com.websocket;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.HandshakeInterceptor;
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
+
+@Configuration
+@EnableWebSocket
+@RequiredArgsConstructor
+public class WebSocketConfig implements WebSocketConfigurer {
+
+	private final WebSocketHandler csmsHandler;
+
+	@Override
+	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+		registry.addHandler(csmsHandler, "/kr/ho")
+			.addInterceptors(new HttpSessionHandshakeInterceptor())
+			.setAllowedOrigins("*");
+	}
+
+}
